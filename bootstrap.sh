51661c2790229e142837d87441aa5c8cecab4770
Greg Sutcliffe <gsutclif@redhat.com>
2014-01-14 13:32:24 +0000

    Use correct repo in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 set -x
 type git || yum -y install git || apt-get -y install git
 git clone https://github.com/sstephenson/bats.git && bats/install.sh /usr/local
-git clone https://github.com/lzap/foreman-bats.git && foreman-bats/install.sh /usr/local
+git clone https://github.com/theforeman/foreman-bats.git && foreman-bats/install.sh /usr/local
