56edccc1cd90c3c1a40f0fd2ffea8372c11d487d
Dominic Cleal <dcleal@redhat.com>
2015-04-20 11:45:22 +0100

    Revert 4a46254 mirror change

--- bootstrap_vagrant.sh
+++ bootstrap_vagrant.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -xe
 type git || yum -y install git || (apt-get update; apt-get -y install git)
-if grep -q ^8 /etc/debian_version ; then
-  # Problem with mirror.rackspace.com
-  echo "deb http://ftp.debian.org/debian/ jessie main contrib non-free\ndeb http://security.debian.org/ jessie/updates main contrib non-free" >/etc/apt/sources.list
-  apt-get update
-fi
 git clone https://github.com/sstephenson/bats.git && bats/install.sh /usr
 /vagrant/install.sh /usr
